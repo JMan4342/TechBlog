b26a3eff2514f981a61a90c48e320c540da8b1e2
Joseph Manning <jgmanning3@yahoo.com>
2021-07-22 23:19:54 -0700

    Code to have userId populate in Comment model.

--- public/js/postDetails.js
+++ public/js/postDetails.js
@@ -6,18 +6,20 @@ const commentFormHandler = async (e) => {
   console.log("inside function");
 
   const commentContent = document.getElementById("commentContent").value.trim();
+  const blogId = document.getElementById("commentPost").dataset.blogId;
 
   if (commentContent) {
 
     const response = await fetch("/api/comment/post/", {
       method: "POST",
       body: JSON.stringify({
+        blogId,
         commentContent,
       }),
       headers: { "Content-Type": "application/json" },
     });
     if (response.ok) {
-      document.location.replace("/");
+      document.location.replace("/postDetails/" + blogId);
     } else {
       alert("Comment failed to post");
     }
